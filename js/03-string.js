7390728605b958442ee160ae09da5c1cb4e8b01e
dittamaz <dittamaz@gmail.com>
2023-05-02 16:38:01 +0300

    string

---
+++ js/03-string.js
@@ -0,0 +1,19 @@
+console.clear();
+
+/*
+STRING tekstas
+
+iniciavimo budai
+- dviguba kabute
+- vienguba kabute
+
+*/
+
+const empty = "";
+const nickname = "Pavardenis";
+const username = 'Vardenis';
+
+console.log(nickname);
+console.log(empty);
+console.log(userename);
+
