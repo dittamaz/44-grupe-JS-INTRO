2fae109a5546f5b8b2404a5791979b50e982d652
dittamaz <dittamaz@gmail.com>
2023-05-05 16:20:55 +0300

    Array

--- js/05-array.js
+++ js/05-array.js
@@ -3,6 +3,9 @@ console.clear();
 /*
 ARRAY - arėjus (slang), sąrašas, listas (slang),
 matrica, masyvas (tikslus vertinys);
+
+Masyvas prasideda index = 0;
+
 */
 
 const empty = [];
@@ -47,3 +50,50 @@ console.log(marks[3]);
 console.log(marks[4]);
 console.log(marks[5]);
 
+
+console.clear();
+
+const marks1 = [10, 9, 8];
+const marks2 = [6, 7, 8];
+const marks3 = [5, 5, 5];
+
+function average(marks) {
+    let sum = 0;
+    sum += marks[0];
+    sum += marks[1];
+    sum += marks[2];
+
+    return sum / 3;
+    // galima parasytine 3,o return sum / marks.length;
+}
+
+console.log('Vidurkis 1:', marks1, '=', average(marks1));
+console.log('Vidurkis 2:', marks2, '=', average(marks2));
+console.log('Vidurkis 3:', marks3, '=', average(marks3));
+
+console.clear();
+
+const dict1 = ['pirmas', 'zodis', 'Labas'];
+const dict2 = ['Labas', 'rytas', 'Lietuva'];
+const dict3 = ['Labas', 'rytas', 'suraitytas'];
+
+// Zodynas : a, b, c.
+
+function dictionary(wordList) {
+    console.log(wordList);
+    let result = 'Zodynas: ';
+
+    // result += wordList[0] + ', ';
+    // result += wordList[1] + ', ';
+    // result += wordList[2] + '.';
+
+    // result += wordList[0];
+    // result += ', ';
+    // ir t.t.
+
+    return `Zodynas: ${wordList[0]}, ${wordList[1]}, ${wordList[2]}.`;
+
+}
+console.log(dictionary(dict1));
+console.log(dictionary(dict2));
+console.log(dictionary(dict3));
