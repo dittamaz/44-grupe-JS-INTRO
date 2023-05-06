c7b806b220d0c630c65d4a908761bb414f246d50
dittamaz <dittamaz@gmail.com>
2023-05-06 07:25:51 +0300

    Boolean

--- js/06-boolean.js
+++ js/06-boolean.js
@@ -4,7 +4,7 @@ console.clear();
 BOOLEAN logine reiksme
 variantai: true, folse.
 klausiamosios formos, atsakymas taip arba ne.
-/*
+
 
 const asSvieciaSaule = true;
 const arNaktis = false;
@@ -12,4 +12,5 @@ const arNaktis = false;
 const arPilnatis = true;
 const arSkanu = false;
 
-const isUser
+
+*/
