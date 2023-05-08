f4b275a837fc9fbc0fd78b7fef179a4400c78a23
dittamaz <dittamaz@gmail.com>
2023-05-08 18:12:48 +0300

    Uzduotys

---
+++ js/00-uzduotys.js
@@ -0,0 +1,21 @@
+console.clear();
+
+const skaiciai = 0;
+let suma = 0;
+
+for (let i = 0; i <= 0; i++) {
+    suma = suma + i;
+};
+console.log(suma);
+
+console.log('------');
+
+const skaiciai1 = 100;
+let suma1 = 0;
+
+for (let i = 0; i <= 100; i++) {
+    suma1 = suma1 + i;
+};
+console.log(suma1);
+
+console.log('------');
--- js/12-for.js
+++ js/12-for.js
@@ -30,34 +30,66 @@ console.clear();
 
 console.log('------------------');
 
-const pazymiai = [10, 2, 8, 4, 6];
+//const pazymiai = [10, 2, 8, 4, 6];
 
-for (let i = 0; i <= 8; i++) {
-    console.log(i);
-};
+//for (let i = 0; i <= 8; i++) {
+//  console.log(i);
+//};
 
 // kaip istraukti konkretu pazymi
 
-for (let i = 0; i < 7; i++) {
-    console.log(i, '-', pazymiai[i]);
-};
+//for (let i = 0; i < 7; i++) {
+ //   console.log(i, '-', pazymiai[i]);
+//};
 
-for (let i = 2; i < 7; i++) {
-    console.log(i, '-', pazymiai[i]);
-};
+//for (let i = 2; i < 7; i++) {
+//    console.log(i, '-', pazymiai[i]);
+//};
 
 
 
 console.log('START');
 
+//           i ->  0   1  2  3  4  5  6  7  8  9
+const pazymiai = [10, 2, 8, 4, 6, 1, 3, 5, 7, 9];
+const kasKelintas = 3;
 
-//const pazymiai = [10, 2, 8]
+for (let i = kasKelintas - 1; i < pazymiai.length; i += kasKelintas) {
+   console.log(i, '-', pazymiai[i]);
+};
+console.log('--------------------------------------');
 
 for (let i = 0; i < pazymiai.length; i++) {
-    console.log(i, '-', marks[i]);
+    console.log(i, '-', pazymiai[i]);
+};
+
+
+console.log('kitaip');
+for (let i = 0; i < pazymiai.length; i++) {
+    if (i % kasKelintas === 0 ) {
+    console.log(i, '-', pazymiai[i]);
+}
+};
+
+console.log('kitaip');
+for (let i = 0; i < pazymiai.length; i++) {
+    if (i % kasKelintas === kasKelintas - 1) {
+    console.log(i, '-', pazymiai[i]);
+}
 };
 
 console.log('END');
 
 console.log('--------------------------------------');
+console.clear();
+
+// -5...6
+
+for (let i = -5; i < 7; i++) {
+    console.log(i);
+};
+
+for (let i = 0; i < 2; i+=0.1) {
+    console.log(i);
+}
 
