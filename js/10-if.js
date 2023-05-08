c54f87853e01915696d39416cd94ead279c51451
dittamaz <dittamaz@gmail.com>
2023-05-08 08:55:33 +0300

    if

--- js/10-if.js
+++ js/10-if.js
@@ -2,10 +2,77 @@ console.clear();
 
 
 /*
+
+Palyginimo operatoriai:
+visi: >, <, >=, <=, ==, ===,
+naudotini: >, <, >=, <=, ==, ===,
+nenaudotini: ==, 
+
+if () {}
+if () {} else {}
+if () {} else if () {}
+if () {} else if () {} ... else if () {}
+if () {} else if () {} ... else if () {} else {}
+
+klausimu 'jeigu', gali buti daug
+
+*/
+
+if (4 > 2) {
+    console.log('taip');
+};
+
 if (7 < 1) {
     console.log('7 < 1');
-}
-else {
-    console.log('7 ne < 1');
-}
-*/
+}  else {
+    console.log('7 ne < uz 1');
+};
+
+const userAge = 10;
+const ageLimit = 18;
+
+if (userAge >= ageLimit) {
+    console.log('uzeik....:)');
+} else {
+    console.log(`dar palauk ${ageLimit - userAge} metus..`);
+};
+
+const diena = 3;
+
+if (diena === 1) {
+    console.log('pirmadienis');
+} else if (diena === 2) {
+    console.log('antradienis');
+} else if (diena === 3) {
+    console.log('treciadienis');
+} else if (diena === 4) {
+    console.log('ketvirtadieni');
+} else if (diena === 5) {
+    console.log('penktadienis');
+} else if (diena === 6) {
+    console.log('sestadienis');
+} else if (diena === 7) {
+    console.log('sekmadienis');
+} else {
+    console.log('tokios dienos nera')
+};
+
+console.log('---------------------------');
+
+// kodo nestinimas
+
+const arSvieciaSaule = true;
+const arLyja = false;
+
+if (arSvieciaSaule) {
+    kaDaryti = 'eisim i kiema';
+    if (arLyja) {
+        kaDaryti = 'ziurim i vaivorikste :0';
+    } else {
+        kaDaryti = 'deginames pleziuke B-)';
+    }
+} else {
+    kaDaryti = 'kodinsim....;-)'
+};
+
+console.log(kaDaryti);
