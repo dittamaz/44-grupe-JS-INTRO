b7e98dd3c7a02a669224c5c7cf58e9e3b98f35cf
dittamaz <dittamaz@gmail.com>
2023-05-08 18:31:44 +0300

    Uzduotys

--- js/00-uzduotys.js
+++ js/00-uzduotys.js
@@ -10,12 +10,32 @@ console.log(suma);
 
 console.log('------');
 
-const skaiciai1 = 100;
+const skaiciai1 = 4;
 let suma1 = 0;
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 0; i <= 4; i++) {
     suma1 = suma1 + i;
 };
 console.log(suma1);
+
+console.log('------');
+
+const skaiciai2 = 100;
+let suma2 = 0;
+
+for (let i = 0; i <= 100; i++) {
+    suma2 = suma2 + i;
+};
+console.log(suma2);
+
+console.log('------');
+
+const skaiciai3 = 815;
+let suma3 = 0;
+
+for (let i = 574; i < 816; i++) {
+    suma3 = suma3 + i;
+};
+console.log(suma3);
 
 console.log('------');
